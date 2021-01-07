4631504761d2bd363205c68f6c87433cfc4b6f23
Phil <parlihbp@gmail.com>
2021-01-07 09:42:20 -0500

    Added printLn for each patient being considered for a trial

--- src/main/kotlin/com/phil/deeplens/service/TrialEvaluator.kt
+++ src/main/kotlin/com/phil/deeplens/service/TrialEvaluator.kt
@@ -9,6 +9,13 @@ class TrialEvaluator {
     fun evaluate(trials: List<ClinicalTrial>, patients: List<Patient>) {
         trials.forEach { trial ->
             println("Processing trial id ${trial.trialId} for condition ${trial.condition} with ${patients.size} potential patients")
+            patients.forEach { patient ->
+                evaluatePatientForTrial(trial, patient)
+            }
         }
+    }
+
+    private fun evaluatePatientForTrial(trial: ClinicalTrial, patient: Patient) {
+        println("Processing patient ${patient.patientId}, age ${patient.age}, gender ${patient.gender}, with diagnosis ${patient.diagnosis}, for trial ${trial.trialId}")
     }
 }
--- src/test/kotlin/com/phil/deeplens/service/TrialEvaluatorTest.kt
+++ src/test/kotlin/com/phil/deeplens/service/TrialEvaluatorTest.kt
@@ -18,15 +18,13 @@ internal class TrialEvaluatorTest {
     lateinit var trialEvaluator: TrialEvaluator
 
 
-    val trials = listOf(
-        ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site", "1|2", ">18"),
-        ClinicalTrial("2", "Title 2", "Description", "3", "condition", "site", "1|2", ">18")
-    )
+    val trial1 = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site", "1|2", ">18")
+    val trial2 = ClinicalTrial("2", "Title 2", "Description", "3", "condition", "site", "1|2", ">18")
+    val trials = listOf(trial1, trial2)
 
-    val patients = listOf(
-        Patient("1", 55, "male", "diag", "site"),
-        Patient("2", 95, "female", "diag", "site")
-    )
+    val patient1 = Patient("1", 55, "male", "diag1", "site1")
+    val patient2 = Patient("2", 95, "female", "diag2", "site2")
+    val patients = listOf(patient1, patient2)
 
     @Test
     fun `evaluate will log trial id, title, and potential patients`() {
@@ -35,9 +33,22 @@ internal class TrialEvaluatorTest {
         }.lines().dropLast(1)
 
 
-        assertThat(lines).containsExactlyInAnyOrder(
+        assertThat(lines).contains(
             "Processing trial id 1 for condition condition with 2 potential patients",
             "Processing trial id 2 for condition condition with 2 potential patients"
+        )
+    }
+
+    @Test
+    fun `evaluate will print patient information`(){
+        val lines = tapSystemOut {
+            trialEvaluator.evaluate(trials.dropLast(1), patients)
+        }.lines().dropLast(1)
+
+        assertThat(lines).containsExactlyInAnyOrder(
+            "Processing trial id 1 for condition condition with 2 potential patients",
+            "Processing patient ${patient1.patientId}, age ${patient1.age}, gender ${patient1.gender}, with diagnosis ${patient1.diagnosis}, for trial ${trial1.trialId}",
+            "Processing patient ${patient2.patientId}, age ${patient2.age}, gender ${patient2.gender}, with diagnosis ${patient2.diagnosis}, for trial ${trial1.trialId}",
         )
     }
 }
