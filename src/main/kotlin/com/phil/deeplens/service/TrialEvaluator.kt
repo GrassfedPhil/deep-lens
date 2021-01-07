dd99c893790a087962ca8bd6bfbfef0bb3e2e860
Phil <parlihbp@gmail.com>
2021-01-07 13:26:53 -0500

    evaluate will now do evaluation against trial criteria and return a list of eligible patients associated with the trial they are eligible for.

--- src/main/kotlin/com/phil/deeplens/service/TrialEvaluator.kt
+++ src/main/kotlin/com/phil/deeplens/service/TrialEvaluator.kt
@@ -6,16 +6,47 @@ import org.springframework.stereotype.Service
 
 @Service
 class TrialEvaluator {
-    fun evaluate(trials: List<ClinicalTrial>, patients: List<Patient>) {
-        trials.forEach { trial ->
+    fun evaluate(trials: List<ClinicalTrial>, patients: List<Patient>): List<Pair<ClinicalTrial, List<Patient>>> {
+        return trials.map { trial ->
             println("Processing trial id ${trial.trialId} for condition ${trial.condition} with ${patients.size} potential patients")
-            patients.forEach { patient ->
+            val eligiblePatients = patients.mapNotNull { patient ->
                 evaluatePatientForTrial(trial, patient)
             }
+            Pair(trial, eligiblePatients)
         }
     }
 
-    private fun evaluatePatientForTrial(trial: ClinicalTrial, patient: Patient) {
+    private fun evaluatePatientForTrial(trial: ClinicalTrial, patient: Patient): Patient? {
         println("Processing patient ${patient.patientId}, age ${patient.age}, gender ${patient.gender}, with diagnosis ${patient.diagnosis}, for trial ${trial.trialId}")
+        if (checkAgeRequirement(trial.ageRequirement, patient.age)
+            && checkDiagnosis(trial.diagnoses, patient.diagnosis)
+            && checkAnatomicSite(trial.anatomicSite, patient.anatomicSite)
+        ) {
+            return patient
+        }
+        return null
+    }
+
+    private fun checkAnatomicSite(trialAnatomicSite: String, patientAnatomicSite: String): Boolean {
+        return trialAnatomicSite.equals(patientAnatomicSite, ignoreCase = true)
+    }
+
+    private fun checkDiagnosis(trialDiagnosis: String, patientDiagnosis: String): Boolean {
+        val diagnoses = trialDiagnosis.split("|").map { it.toLowerCase() }
+        return diagnoses.contains(patientDiagnosis.toLowerCase())
+    }
+
+    private fun checkAgeRequirement(ageRequirement: String, age: Int): Boolean {
+        val (operator, ageReq) = Regex("(\\D*)(\\d*)").find(ageRequirement)!!.destructured
+        return when (operator) {
+            ">" -> age > ageReq.toInt()
+            "<" ->  age < ageReq.toInt()
+            ">=" -> age >= ageReq.toInt()
+            "<=" -> age <= ageReq.toInt()
+            else -> {
+                println("OPERATOR DID NOT MATCH!!")
+                false
+            }
+        }
     }
 }
--- src/test/kotlin/com/phil/deeplens/service/TrialEvaluatorTest.kt
+++ src/test/kotlin/com/phil/deeplens/service/TrialEvaluatorTest.kt
@@ -18,16 +18,18 @@ internal class TrialEvaluatorTest {
     lateinit var trialEvaluator: TrialEvaluator
 
 
-    val trial1 = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site", "1|2", ">18")
-    val trial2 = ClinicalTrial("2", "Title 2", "Description", "3", "condition", "site", "1|2", ">18")
-    val trials = listOf(trial1, trial2)
+    val trial1 = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site1", "ONE|TWO", ">18")
+    val trial2 = ClinicalTrial("2", "Title 2", "Description", "3", "condition", "site2", "One|Two", ">18")
 
-    val patient1 = Patient("1", 55, "male", "diag1", "site1")
-    val patient2 = Patient("2", 95, "female", "diag2", "site2")
-    val patients = listOf(patient1, patient2)
+    val patient1 = Patient("1", 55, "male", "one", "Site1")
+    val patient2 = Patient("2", 95, "female", "two", "sIte2")
+    val patient3 = Patient("3", 15, "female", "two", "sitE2")
+    val patient4 = Patient("4", 65, "male", "two", "siTe1")
 
     @Test
     fun `evaluate will log trial id, title, and potential patients`() {
+        val trials = listOf(trial1, trial2)
+        val patients = listOf(patient1, patient2)
         val lines = tapSystemOut {
             trialEvaluator.evaluate(trials, patients)
         }.lines().dropLast(1)
@@ -40,9 +42,11 @@ internal class TrialEvaluatorTest {
     }
 
     @Test
-    fun `evaluate will print patient information`(){
+    fun `evaluate will print patient information`() {
+        val trials = listOf(trial1)
+        val patients = listOf(patient1, patient2)
         val lines = tapSystemOut {
-            trialEvaluator.evaluate(trials.dropLast(1), patients)
+            trialEvaluator.evaluate(trials, patients)
         }.lines().dropLast(1)
 
         assertThat(lines).containsExactlyInAnyOrder(
@@ -50,5 +54,55 @@ internal class TrialEvaluatorTest {
             "Processing patient ${patient1.patientId}, age ${patient1.age}, gender ${patient1.gender}, with diagnosis ${patient1.diagnosis}, for trial ${trial1.trialId}",
             "Processing patient ${patient2.patientId}, age ${patient2.age}, gender ${patient2.gender}, with diagnosis ${patient2.diagnosis}, for trial ${trial1.trialId}",
         )
+    }
+
+    @Test
+    fun `checkAgeRequirement functions as expected`() {
+        val greaterThan = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site1", "1|2", ">18")
+        val greaterThanEqual = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site1", "1|2", ">=18")
+        val lesserThan = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site1", "1|2", "<18")
+        val lesserThanEqual = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site1", "1|2", "<=18")
+        val unknown = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site1", "1|2", "=18")
+        val unknown2 = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site1", "1|2", "%jjjj")
+        val blank = ClinicalTrial("1", "Title 1", "Description", "3", "condition", "site1", "1|2", "")
+
+        val nineteen = Patient("1", 19, "male", "1", "site1")
+        val eighteen = Patient("1", 18, "male", "1", "site1")
+        val seventeen = Patient("1", 17, "male", "1", "site1")
+        val patients = listOf(seventeen, eighteen, nineteen)
+
+        val evaluateGreaterThan = trialEvaluator.evaluate(listOf(greaterThan), patients)
+        assertThat(evaluateGreaterThan[0].second).containsExactlyInAnyOrder(nineteen)
+
+        val evaluateGreaterThanEqual = trialEvaluator.evaluate(listOf(greaterThanEqual), patients)
+        assertThat(evaluateGreaterThanEqual[0].second).containsExactlyInAnyOrder(nineteen, eighteen)
+
+        val evaluateLesserThan= trialEvaluator.evaluate(listOf(lesserThan), patients)
+        assertThat(evaluateLesserThan[0].second).containsExactlyInAnyOrder(seventeen)
+
+        val evaluateLesserThanEqual = trialEvaluator.evaluate(listOf(lesserThanEqual), patients)
+        assertThat(evaluateLesserThanEqual[0].second).containsExactlyInAnyOrder(seventeen, eighteen)
+
+        val evaluateUnknown = trialEvaluator.evaluate(listOf(unknown), patients)
+        assertThat(evaluateUnknown[0].second).isEmpty()
+
+        val evaluateUnknown2 = trialEvaluator.evaluate(listOf(unknown2), patients)
+        assertThat(evaluateUnknown2[0].second).isEmpty()
+
+        val evaluateBlank = trialEvaluator.evaluate(listOf(blank), patients)
+        assertThat(evaluateBlank[0].second).isEmpty()
+
+    }
+
+    @Test
+    fun `evaluate will evaluate patients and return those that meet the trial criteria`() {
+        val trials = listOf(trial1)
+        val patients = listOf(patient1, patient2, patient3, patient4)
+
+        val completedRun = trialEvaluator.evaluate(trials, patients)
+        assertThat(completedRun).hasSize(1)
+        assertThat(completedRun[0].first).isEqualTo(trial1)
+        assertThat(completedRun[0].second).containsExactlyInAnyOrder(patient1, patient4)
+
     }
 }
